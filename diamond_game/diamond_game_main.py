0fcf3ae4a677895824ceb12e42be246308d1a6fd
Bato-Bair Tsyrenov <batousik@yandex.ru>
2015-03-13 02:24:10 +0000

    [Issues closed] BG-6

--- diamond_game/diamond_game_main.py
+++ diamond_game/diamond_game_main.py
@@ -9,15 +9,15 @@ Then starts small pygame example
 
 def example():
     pygame.init()  # load pygame modules
-    size = width, height = 320, 240  # size of window
-    speed = [2, 2]  # speed and direction
+    size = width, height = 1000, 600  # size of window
+    speed = [99, 31]  # speed and direction
     screen = pygame.display.set_mode(size)  # make window
     s = pygame.Surface((100, 50))  # create surface 100px by 50px
     s.fill((33, 66, 99))  # color the surface blue
     r = s.get_rect()  # get the rectangle bounds for the surface
     clock = pygame.time.Clock()  # make a clock
     while 1:  # infinite loop
-        clock.tick(30)  # limit framerate to 30 FPS
+        clock.tick(100)  # limit framerate to 100 FPS
         for event in pygame.event.get():  # if something clicked
                 if event.type == pygame.QUIT:  # if EXIT clicked
                         sys.exit()  # close cleanly
