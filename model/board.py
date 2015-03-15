9a0feee3f9cf215516f8a9d327f9e3f53c4b1a14
Bato-Bair Tsyrenov <batousik@yandex.ru>
2015-03-15 06:46:14 +0000

    Initialised chinese checkers board to start state

--- model/board.py
+++ model/board.py
@@ -1,19 +1,15 @@
-SIZE_PLAYER_BASE = 4
+SIZE_PLAYER_BASE = 10
+SIZE_PLAYER_BASE_GRID = (SIZE_PLAYER_BASE * 2) - 1
 SIZE_BOARD_CENTER = SIZE_PLAYER_BASE + 1
 SIZE_TRIANGLE_BASE = (SIZE_PLAYER_BASE * 3) + 1
-SIZE_BOARD_X = (SIZE_TRIANGLE_BASE * 2) - 1
-SIZE_BOARD_Y = (SIZE_PLAYER_BASE * 4) + 1
-
-
-# def init_board():
-#     for y in (0, BOARD_SIZE_Y):
-#         for x in (0, BOARD_SIZE_X):
-#             board[y][x]
+SIZE_TRIANGLE_BASE_GRID = (SIZE_TRIANGLE_BASE * 2) - 1
+SIZE_BOARD_X_GRID = (SIZE_TRIANGLE_BASE * 2) - 1
+SIZE_BOARD_Y_GRID = (SIZE_PLAYER_BASE * 4) + 1
 
 
 def mrow(num_fields):
     empties = num_fields-1
-    non_playable = (SIZE_BOARD_X - num_fields - empties)/2
+    non_playable = (SIZE_BOARD_X_GRID - num_fields - empties)/2
     margin = [-1] * non_playable
     center = []
     for i in range(1, num_fields + empties + 1):
@@ -29,7 +25,7 @@ def make_board():
     cnt = 0
     last_ind = 0
 
-    for i in range(1, SIZE_BOARD_Y+1):
+    for i in range(1, SIZE_BOARD_Y_GRID+1):
         if i/SIZE_BOARD_CENTER == 0:
             board.append(mrow(i))
         elif i/SIZE_BOARD_CENTER == 1:
@@ -43,23 +39,43 @@ def make_board():
     return board
 
 
-# def init_board(board):
-#     for i in range(SIZE_BOARD_Y):
-#         row = board[i]
-#         if i/SIZE_BOARD_CENTER == 0:
-#             board.append(mrow(i))
-#         elif i/SIZE_BOARD_CENTER == 1:
-#             board.append(mrow(SIZE_TRIANGLE_BASE-cnt))
-#             cnt += 1
-#             last_ind = len(board) - 1
-#         else:
-#             last_ind -= 1
-#             board.append(list(board[last_ind]))
+def init_board(board):
+    i = SIZE_PLAYER_BASE_GRID
+    for y in range(SIZE_BOARD_Y_GRID):
+        row = board[y]
+        if y/SIZE_PLAYER_BASE == 0:
+            for x in range(SIZE_BOARD_X_GRID):
+                if row[x] == 0:
+                    row[x] = 1
+        elif y/SIZE_PLAYER_BASE == 1:
+            for x in range(SIZE_BOARD_X_GRID):
+                if row[x] == 0:
+                    if x < i:
+                        row[x] = 2
+                    elif x + i >= SIZE_TRIANGLE_BASE_GRID:
+                        row[x] = 3
+            i -= 1
+        elif (y-1)/SIZE_PLAYER_BASE == 2:
+            i += 1
+            for x in range(SIZE_BOARD_X_GRID):
+                if row[x] == 0:
+                    if x < i:
+                        row[x] = 4
+                    elif x + i >= SIZE_TRIANGLE_BASE_GRID:
+                        row[x] = 5
+
+        elif (y-1)/SIZE_PLAYER_BASE == 3:
+            for x in range(SIZE_BOARD_X_GRID):
+                if row[x] == 0:
+                    row[x] = 6
+
+    return board
+
 
 def print_board(board):
     line = ''
-    for y in range(SIZE_BOARD_Y):
-        for x in range(SIZE_BOARD_X):
+    for y in range(SIZE_BOARD_Y_GRID):
+        for x in range(SIZE_BOARD_X_GRID):
             if board[y][x] == -1:
                 line += '_'
             elif board[y][x] == 0:
@@ -78,4 +94,4 @@ if __name__ == "__main__":
     # print len(get_row(7))
     # print_board(make_board())
 
-    print_board(make_board())
+    print_board(init_board(make_board()))
