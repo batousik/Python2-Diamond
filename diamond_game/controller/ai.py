632da422c97a685dfc9a3dc955e544a51861b7f1
Bato-Bair Tsyrenov <batousik@yandex.ru>
2015-04-21 19:58:02 +0100

    v 0.9 - final

--- diamond_game/controller/ai.py
+++ diamond_game/controller/ai.py
@@ -1,6 +1,8 @@
 import sys
 import traceback
 import time
+import copy
+import math
 from diamond_game import *
 from diamond_game.model.models import Board
 import random
@@ -49,9 +51,13 @@ class AI(MVCObject):
         self.player = data['player']
         self.pieces = {}
         self.find_pieces_and_moves()
-        self.random_move()
+        if Conf.OPT_OPTIONS.get(Conf.AI_DIF) == Conf.OPT_EASY:
+            self.random_move()
+        elif Conf.OPT_OPTIONS.get(Conf.AI_DIF) == Conf.OPT_MEDIUM:
+            self.better_move()
 
     def find_pieces_and_moves(self):
+        self.pieces = {}
         for x in range(self.board.SIZE_BOARD_X_GRID):
             for y in range(self.board.SIZE_BOARD_Y_GRID):
                 if self.board.get_field((x, y)).value == self.player:
@@ -119,3 +125,125 @@ class AI(MVCObject):
             data = {'skip': 1}
         self.post(AIMovedEvent(data), Conf.MODEL)
 
+    def better_move(self):
+        datas = []
+        cpus = []
+        try:
+            if len(self.pieces) > 0:
+                for key, value in self.pieces.iteritems():
+                    if len(value) > 0:
+                        for loc in value:
+                            cpu = CrazyCPU(key, loc, copy.deepcopy(self.board), datas, self.player)
+                            cpus.append(cpu)
+                            cpu.start()
+
+                waiting = 1
+                while waiting:
+                    x = 0
+                    for cpu in cpus:
+                        if cpu.is_alive():
+                            x = 1
+                    waiting = x
+
+                smallest = 1001.0
+                for a_val in datas:
+                    if a_val.val < smallest:
+                        smallest = a_val.val
+                        start, end = a_val.loc_s, a_val.loc_e
+
+                data = {'skip': 0, 'start': start, 'end': end}
+                print datas
+            else:
+                # In case there are no moves
+                data = {'skip': 1}
+            self.post(AIMovedEvent(data), Conf.MODEL)
+        except:
+            e = sys.exc_info()[0]
+            print '>>>>>>>>>>> Fatal Error in: CRAXZY CPUSSSSSSSSSS'
+            print e
+            traceback.print_exc()
+            # fight back
+            data = {'skip': 1}
+            self.post(AIMovedEvent(data), Conf.MODEL)
+
+
+class Data(object):
+    def __init__(self, val, loc_s, loc_e):
+        self.val = val
+        self.loc_s = loc_s
+        self.loc_e = loc_e
+
+    def __str__(self):
+        return str(self.val) + " " + str(self.loc_s) + " " + str(self.loc_e)
+
+    def __repr__(self):
+        return self.__str__()
+
+class CrazyCPU(AI):
+    cnt = 0
+
+    def __init__(self, loc_s, loc_e, board_copy, data_list, player):
+        AI.__init__(self, EventManager())
+        self.board = board_copy
+        self.data_list = data_list
+        self.loc_s = loc_s
+        self.loc_e = loc_e
+        CrazyCPU.cnt += 1
+        self.cnt = CrazyCPU.cnt
+        if Conf.DEBUG:
+            print '[CRAZY CPU #' + str(self.cnt) + ' STARTED]'
+        self.free_home = []
+        self.free_home2 = []
+        self.player = player
+        self.shortest = 100
+
+    def run(self):
+        self.make_free_home()
+        self.make_move(self.loc_s, self.loc_e)
+        self.make_free_home2()
+        # if len(self.free_home2) < len(self.free_home):
+        #     self.data_list.append(Data(0, self.loc_s, self.loc_e))
+        # else:
+        self.find_pieces_and_moves()
+        self.data_list.append(Data(self.find_shortest(), self.loc_s, self.loc_e))
+        if Conf.DEBUG:
+            print '[CRAZY CPU #' + str(self.cnt) + ' FINISHED]'
+
+    def make_free_home(self):
+        for loc in self.board.win_sectors.get(self.player):
+            self.free_home.append(loc)
+
+    def make_free_home2(self):
+        for loc in self.board.win_sectors.get(self.player):
+            self.free_home2.append(loc)
+
+    def make_move(self, start_loc, end_loc):
+        """Change actual model values for pieces stored.
+        :param start_loc: location(model based).
+        :param end_loc: location(model based).
+        Method swaps pieces at given locations.
+        """
+        temp = self.board.get_field(start_loc)
+        self.board.set_field(start_loc, self.board.get_field(end_loc))
+        self.board.set_field(end_loc, temp)
+
+    def find_shortest(self):
+        valr = 200
+        print self.free_home2
+        print self.pieces
+        if len(self.pieces) > 0:
+            for key, value in self.pieces.iteritems():
+                if key not in self.board.win_sectors[self.player]:
+                    for home_loc in self.free_home2:
+                        dx = abs(key[0]-home_loc[0])
+                        dy = abs(key[1]-home_loc[1])
+                        val = math.sqrt(dx*dx+dy*dy)
+                        print val
+                        print self.loc_s
+                        print self.loc_e
+                        if 1.41 < val < 1.42:
+                            val = 12
+                            valr = val
+
+                        valr += val
+        return valr
